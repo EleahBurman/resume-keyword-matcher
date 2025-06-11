88e822d131bc89b4e038053e2477ced8f2c087dc
eleahburman <eleahjburman@gmail.com>
2025-06-10 21:57:17 -0400

    Use different port for testing.

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ if __name__ == '__main__':
     
     Note: Only use debug=True in development, never in production!
     """
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
